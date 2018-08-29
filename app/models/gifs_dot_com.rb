2cc4b8f5b211c5ada7fc51e9ea3f7c8fd547fb16
Agustin Beamurguia <agus.beam@gmail.com>
2018-08-29 18:15:15 -0300

    first commit

---
+++ app/models/gifs_dot_com.rb
@@ -0,0 +1,26 @@
+class GifsDotCom
+  include HTTParty
+  base_uri 'https://api.gifs.com'
+  debug_output $stdout # <= will spit out all request details to the console
+
+  def initialize(key)
+    @key = 'gifs56d63999f0f34'
+  end
+
+  def import(source, trim)
+    self.class.post("/media/import", { body: { source: "https://www.youtube.com/watch?v=jhyANGHDDH8", trim: { start: 118, end: 122 } }.to_json, headers: { 'Gifs-API-Key' => @key, 'Content-Type' => 'application/json' }})
+  end
+end
+# curl -X POST -H "Gifs-API-Key: gifs56d63999f0f34" -H "Content-Type: application/json" -d '{ "source": "https://www.youtube.com/watch?v=jhyANGHDDH8", "trim": { "start": 118, "end": 122 } }' "https://api.gifs.com/media/import"
+# {
+#   "success": {
+#     "page": "https://gifs.com/gif/APVWzz",
+#     "oembed": "https://gifs.com/oembed/APVWzz",
+#     "embed": "<iframe src='https://gifs.com/embed/APVWzz' frameborder='0' scrolling='no' width='640' height='360' style='-webkit-backface-visibility: hidden;-webkit-transform: scale(1);' ></iframe>",
+#     "files": {
+#       "gif": "https://j.gifs.com/APVWzz.gif",
+#       "jpg": "https://j.gifs.com/APVWzz.jpg",
+#       "mp4": "https://j.gifs.com/APVWzz.mp4"
+#     }
+#   }
+# }
