dddd2be60112e6ed3fd85901ebc70dce30781085
Pablo Soldi <pablo@robly.com>
2018-08-30 15:04:26 -0700

    Add automatic reload video

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,16 +1,21 @@
 $( document ).ready(function() {
+  const player = new Plyr('#plyr-youtube', { autoplay: true });
+
   //plyr.setup("#plyr-youtube");
 
 	$("#link").bind("paste", function(e){
-	    setTimeout(function () {
-	    	$.get( "/home/youtube", { url: $('#link-input').val() } )
-				  .done(function( data ) {
-				  	$('#iframe-video').attr('src', $('#link-input').val());
-				    $('.card').show();
-				    $('.card-title').text(data.title);
-				    $('#duration').attr('max', data.duration);
-				    
-				  });
-    }, 100);
-	} );
+    setTimeout(function () {
+      $('#video-div').html('<div id="plyr-youtube" data-plyr-provider="youtube" data-plyr-embed-id=""></div>');
+      $.get( "/home/youtube", { url: $('#link-input').val() } )
+        .done(function( data ) {
+          $('.card').show();
+          $('.card-title').text(data.title);
+          $('#duration').attr('max', data.duration);
+          $('#plyr-youtube').attr('data-plyr-embed-id', data.id);
+          new Plyr('#plyr-youtube', {
+            autoplay: true
+          });
+        });
+    },100);
+  });
 });
