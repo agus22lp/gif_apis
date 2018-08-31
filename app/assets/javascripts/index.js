ceb5bd0a2ea1b8fd751857c7928c62ab55d55f1b
Pablo Soldi <pablo@robly.com>
2018-08-31 11:05:30 -0700

    imitate gif-loop with callbacks

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,8 +1,47 @@
 $( document ).ready(function() {
-  const player = new Plyr('#plyr-youtube', { autoplay: true });
+  const player = new Plyr('#plyr-youtube', {
+    debug: true,
+    controls: [],
+    muted: true,
+    clickToPlay: false,
+    //resetOnEnd: true,
+    autoplay: true,
+    fullscreen: { enabled: false }
+  });
+
+
+
+  
+  //imitate loop with callbacks
+  player.on('ready', event => {
+    event.detail.plyr.currentTime = 170;
+  });
+
+  player.on('timeupdate', event => {
+    if(event.detail.plyr.currentTime >= 175){
+      event.detail.plyr.currentTime = 170;
+    }
+  });
+
+
+
+
+
+  $("#link-submit").on('click',function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    //player.currentTime = 15;
+    //player.duration = 10;
+
+    player.play();
+
+    return false;
+  });
+
 
   //plyr.setup("#plyr-youtube");
-
+/*
 	$("#link").bind("paste", function(e){
     setTimeout(function () {
       $('#video-div').html('<div id="plyr-youtube" data-plyr-provider="youtube" data-plyr-embed-id=""></div>');
@@ -18,4 +57,5 @@ $( document ).ready(function() {
         });
     },100);
   });
+*/
 });
