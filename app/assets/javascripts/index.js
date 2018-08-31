6a687f7067897a5a3bd71c96c29e71542ac86165
Agustin Beamurguia <agus.beam@gmail.com>
2018-08-31 18:30:32 -0300

    heroku and gallery

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -26,7 +26,7 @@ $( document ).ready(function() {
     event.detail.plyr.currentTime = startTime;
   });
 
-  const jrange = $('.range-slider').jRange({
+  jrange = $('.range-slider').jRange({
     from: 0,
     to: 100,
     step: 1,
@@ -36,9 +36,9 @@ $( document ).ready(function() {
     isRange : true,
     ondragend: function(value){
     	arrayVal = value.split(',');
-    	//if ((startTime != parseInt(arrayVal[0]) || (gifTime != parseInt(arrayVal[1]))) {
-
-    	//}
+    	if ((startTime != parseInt(arrayVal[0])) || (gifTime != parseInt(arrayVal[1]))) {
+    		player.currentTime = parseInt(arrayVal[0]);
+    	}
     	startTime = parseInt(arrayVal[0]);
     	gifTime = parseInt(arrayVal[1]);
     }
@@ -49,23 +49,13 @@ $( document ).ready(function() {
 
 	$("#link-input").bind("paste", function(e){
     setTimeout(function () {
-      $('#video-div').html('<div id="plyr-youtube" data-plyr-provider="youtube" data-plyr-embed-id=""></div>');
       $.get( "/home/youtube", { url: $('#link-input').val() } )
         .done(function( data ) {
           $('.video-details').removeClass('d-none');
           $('.video-title').text(data.title);
-          //$('#duration').attr('max', data.duration);
           $('#plyr-youtube').attr('data-plyr-embed-id', data.id);
-          player.source = {
-					    type: 'video',
-					    sources: [
-					        {
-					            src: 'bTqVqk7FSmY',
-					            provider: 'youtube',
-					        },
-					    ],
-					};
-          console.log(player);
+          $('.range-slider').jRange('updateRange', '0,'+ data.duration.toString(), '0,10');
+          player.play();
         });
     },100);
   });
