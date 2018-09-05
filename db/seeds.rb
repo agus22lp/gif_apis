477e4e55ec8588a56efd005012d2577664b8ff8c
Pablo Soldi <pablo@robly.com>
2018-09-05 12:17:20 -0700

    Add generated gifs to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Giphy.create([{ api_id: '14xhcyeSNjhm58uPOh' }, { api_id: 'sRHZenkkwXK2Ka0viT' }])
