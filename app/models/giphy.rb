3f3860a53a00c54db35723ac3657f027accf3391
Pablo Soldi <pablo@robly.com>
2018-09-06 07:18:59 -0700

    refactor giphy, now is working but we cannot trim videos

--- app/models/giphy.rb
+++ app/models/giphy.rb
@@ -1,10 +1,11 @@
 class Giphy < ApplicationRecord
   #debug_output $stdout # <= will spit out all request details to the console
-  #GIPHY_URL = 'https://giphy.com/gifs/'
 
   def self.import(source, trim)
-    response = HTTParty.post("https://giphy.com/gifs/v1/gifs", { query: { source_image_url: source, api_key: TestGif::Application.credentials.giphy }})
-    data = HTTParty.get("https://api.giphy.com/v1/gifs/#{response["id"]}", { query: { api_key: TestGif::Application.credentials.giphy }}).parsed_response["data"]
+    # trim: trim /we don't have trim
+    response = HTTParty.post("https://upload.giphy.com/v1/gifs", { query: { source_image_url: source, api_key: TestGif::Application.credentials.giphy }})
+
+    data = HTTParty.get("https://api.giphy.com/v1/gifs/#{response.parsed_response["data"]["id"]}", { query: { api_key: TestGif::Application.credentials.giphy }}).parsed_response["data"]
 
     self.create(api_id: data["id"], slug: data["slug"], url: data["url"], bitly_url: data["bitly_url"], embed_url: data["embed_url"], username: data["username"],
                 source: data["source"], source_tld: data["source_tld"], source_post_url: data["source_post_url"], title: data["title"])
