aff081cf5e99fcbcba2690da2656e32db82a5b91
Pablo Soldi <pablo@robly.com>
2018-09-07 14:33:22 -0700

    Add gifs class, add spinner in upload file

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -3,7 +3,7 @@ class Gif < ApplicationRecord
   def self.import(source, trim)
     data = HTTParty.post("https://api.gifs.com/media/import", { body: { source: source, trim: trim}.to_json,
       headers: { 'Gifs-API-Key' => TestGif::Application.credentials.gifs_dot_com, 'Content-Type' => 'application/json' }})
-    
+
     if data.parsed_response['success']
       api_id = URI.parse(data.parsed_response['success']['page']).path.split('/').last
       self.create(api_id: api_id, url: data.parsed_response['success']['files']['gif'], mp4: data.parsed_response['success']['files']['mp4'])
@@ -13,3 +13,4 @@ class Gif < ApplicationRecord
   end
 
 end
+  
